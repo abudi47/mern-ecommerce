f37eb03de53b697a6733a64f729630f1b4816bf3
abudi47 <abduselamt47@gmail.com>
2025-07-19 22:17:25 +0300

    Implement coupon functionality: add getCoupon and validateCoupon methods; update routes to include validation

--- backend/controller/coupon.controller.js
+++ backend/controller/coupon.controller.js
@@ -1,1 +1,42 @@
-export const getCoupon = async (req, res) => {};
+import Coupon from "../models/coupon.model.js";
+
+export const getCoupon = async (req, res) => {
+  try {
+    const coupon = await Coupon.findOne({
+      userId: req.user._id,
+      isActive: true,
+    }); //
+
+    res
+      .status(200)
+      .json(coupon ? coupon : { message: "No active coupon found" });
+  } catch (error) {
+    console.log("Error in getCoupon:", error.message);
+    res.status(500).json({ message: error.message || "INTERNAL SERVER ERROR" });
+  }
+};
+
+export const validateCoupon = async (req, res) => {
+  const { code } = req.body;
+
+  const coupon = await Coupon.findOne({
+    code: code,
+    userId: req.user._id,
+    isActive: true,
+  });
+
+  if (!coupon) {
+    return res.status(404).json({ message: "Coupon not found or inactive" });
+  }
+  if (coupon.expiryDate < new Date()) {
+    coupon.isActive = false;
+    await coupon.save();
+    return res.status(400).json({ message: "Coupon has expired" });
+  }
+
+  res.status(201).json({
+    message: "coupon is valid",
+    code: coupon.code,
+    discountPercentage: coupon.discountPercentage,
+  });
+};
--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { protectedRoute } from "../middleware/auth.middleware";
+import { protectedRoute } from "../middleware/auth.middleware.js";
+import { getCoupon, validateCoupon } from "../controller/coupon.controller.js";
 const router = express.Router();
 
-router.get("/", protectedRoute, getCoupon)
+router.get("/", protectedRoute, getCoupon);
+router.get("/", protectedRoute, validateCoupon);
+
 
 export default router;
