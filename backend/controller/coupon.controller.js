55c4b33a964e0279e3261c79f9362c931f8967dd
abudi47 <abduselamt47@gmail.com>
2025-07-19 21:57:02 +0300

    Add coupon functionality: create coupon model, controller, and routes for managing coupons

---
+++ backend/controller/coupon.controller.js
@@ -0,0 +1,1 @@
+export const getCoupon = async (req, res) => {};
--- backend/models/cart.model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-const cartSchema = new mongoose.Schema({}, { timestamps: true });
-
-const Cart = mongoose.model("Cart", cartSchema);
-export default Cart;
